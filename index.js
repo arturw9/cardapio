c906a285e543e90b4a8579cbc084481ddd2b545e
FidelisArtur <arturfidelis@pactosolucoes.com.br>
2022-01-21 10:56:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function Bebidas(){
+    <div class="content">
+      
+        <ul>
+            <div class="card">
+                <div class="topCard">
+                    <h2 class="title">CocaCola 2Lt</h2>
+                    <span class="secondText">Mais de 2 und, taxa de entrega FREE</span>
+                </div>
+                <div class="mediaCard"></div>
+                <div class="bottomCard">
+                    <p class="bottomText">Refri de qualidade, geladinho pra não te deixar na mão!</p>
+                    <div class="actionsCard">
+                        <button class="actions">Curtir</button>
+                        <button class="actions">Compartilhar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="topCard">
+                    <h2 class="title">Guaraná 2Lt</h2>
+                    <span class="secondText">Mais de 2 und, taxa de entrega FREE</span>
+                </div>
+                <div class="mediaCard2"></div>
+                <div class="bottomCard">
+                    <p class="bottomText">Refri de qualidade, geladinho pra não te deixar na mão!</p>
+                    <div class="actionsCard">
+                        <button class="actions">Curtir</button>
+                        <button class="actions">Compartilhar</button>
+                    </div>
+                </div>
+            </div>
+        </ul>
+       
+    </div>
+}
